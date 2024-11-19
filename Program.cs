b7ab6a6617b71dfa7b4c8ba4d0e7f2c5346f6bad
b0gd4n2001 <lulelarub@gmail.com>
2024-11-19 13:36:02 +0200

    Hello Jr. Mind Program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace JuniorMindTemplate
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Jr. Mind");
         }
     }
 }
